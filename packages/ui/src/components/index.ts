36db0e58b967fab8dc8e6978418023ed1a91a345
dogaruemiliano <dogaru.emiliano@gmail.com>
2026-07-06 00:46:59 +0300

    feat(ui): add responsive calendar picker

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -3,6 +3,7 @@ export * from "./alert";
 export * from "./avatar";
 export * from "./badge";
 export * from "./button";
+export * from "./calendar-picker";
 export * from "./card";
 export * from "./checkbox";
 export * from "./country-select";
